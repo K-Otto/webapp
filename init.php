55f247703ce7fdf7a164e4cd987a78d4d1a660f7
Karl <karl1256@yahoo.com>
2016-08-26 20:36:34 +0200

    test

--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php
 $db_name = "webappdb";
-$mysql_user = "root";
-$mysql_pass = "";
-$server_name = "localhost";
+$mysql_user = "adminSZuJWUS";
+$mysql_pass = "ShN5udB5HJax";
+$server_name = "php-beekeeperdomain.rhcloud.com";
 
 $con = mysqli_connect($server_name, $mysql_user, $mysql_pass, $db_name);
 if(!$con)
